fbbc0a848378bdda430173f828ec9f7a70d89e11
vinis-caixe <vinis_caixetabaixo@hotmail.com>
2023-11-18 11:06:47 -0300

    Main view controller

--- src/module-info.java
+++ src/module-info.java
@@ -2,5 +2,8 @@ module workshop_javafx_jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
 	
+	exports gui;
+	opens gui to javafx.fxml;
+	
 	opens application to javafx.graphics, javafx.fxml;
 }
