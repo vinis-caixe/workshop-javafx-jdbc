cf1da6ef1e7dae2bdb45746518b12430af813d0e
vinis-caixe <vinis_caixetabaixo@hotmail.com>
2023-11-19 14:04:14 -0300

    DepartmentService

--- src/module-info.java
+++ src/module-info.java
@@ -5,5 +5,8 @@ module workshop_javafx_jdbc {
 	exports gui;
 	opens gui to javafx.fxml;
 	
+	exports model.entities;
+	opens model.entities to javafx.base;
+	
 	opens application to javafx.graphics, javafx.fxml;
 }
